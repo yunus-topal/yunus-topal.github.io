dc5abc0ed9cb3642dd23071383bec7c3e9fe0608
Yunus Emre Topal <emretopal20@gmail.com>
2025-08-29 02:58:43 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const siteConfig = {
   },
   aboutMe:
     "I’m a software engineer with a Master’s in Informatics (specializing in Computer Vision) from the Technical University of Munich (TUM) and a B.Sc. in Computer Engineering from Boğaziçi University. I have previous work experience building, containerizing, and deploying backend services using .NET and Spring Boot. For the last two years, I’ve been working at Holo-Light, crafting immersive XR experiences in Unity and MRTK.",
-  skills: ["Unity", "C#", ".NET", "C++", "Java", "Python", "Docker", "XR", "Deep Learning", "Computer Vision", "MQTT"],
+  skills: ["Unity", "C#", ".NET", "C++", "Java", "Python", "Docker", "React", "Javascript", "XR", "Deep Learning", "Computer Vision", "MQTT"],
   projects: [
     {
       name: "Human-LLM Interaction Evaluation Framework",
@@ -30,8 +30,29 @@ export const siteConfig = {
       name: "Decoration Game (VR)",
       description:
         "Developed a serious game about Chinese cultural heritage in Unity for Meta Quest 3 and Varjo XR-3",
-      link: "https://extensionkit.io/?ref=devportfolio",
+      link: "https://github.com/yunus-topal",
       skills: ["Unity", "XRI", "C#", "Whisper"],
+    },
+    {
+      name: "Generalized 3D-LMNet",
+      description:
+        "Replicated and extended the 3D-LMNet architecture to perform 3D object reconstruction from single-view 2D images.",
+      link: "https://github.com/yunus-topal/Generalized-3D-LMNet",
+      skills: ["PyTorch", "Python", "Deep Learning", "3D Computer Vision", "Model Generalization"],
+    },
+    {
+      name: "Sudoku",
+      description:
+        "Developed a Sudoku mobile application using Unity and C#.",
+      link: "https://github.com/yunus-topal/Sudoku",
+      skills: ["Unity", "C#", "Mobile Development", "UI/UX Design"],
+    },
+    {
+      name: "Minesweeper",
+      description:
+        "Developed a Minesweeper mobile application using Unity and C#.",
+      link: "https://github.com/yunus-topal/MineSweeper",
+      skills: ["Unity", "C#", "Mobile Development", "UI/UX Design"],
     },
   ],
   experience: [
@@ -40,9 +61,22 @@ export const siteConfig = {
       title: "Software Developer",
       dateRange: "Oct 2023 - Present",
       bullets: [
-        "Working on the Research team, developing the XR applications for the Horizon Europe projects.",
-        "Collaborating with 8+ partner companies on Horizon Europe projects and showing the results at international conferences.",
-        "Customized and integrated Holo-Light Stream for a pre-market Hitachi XR device.",
+        "Optimized SDKs and streaming pipelines in C++ for enterprise projects, improving system performance and enabling smooth data delivery in distributed environments.",
+        "Designed and implemented backend services using FastAPI and RabbitMQ for testing endpoints, managing broker connections, and handling Server-Sent Events (SSE) integrations.",
+        "Integrated third-party APIs and message brokers, collaborating with 8+ partner companies to enable seamless data exchange.",
+        "Implemented secure authentication flows with OAuth 2.0, ensuring reliable and scalable integration across systems."
+      ],
+    },
+    {
+      company: "Cropy AI",
+      title: "Software Developer",
+      dateRange: "Oct 2023 - Present",
+      bullets: [
+        "Built a FastAPI service that extracts frames from video (FFmpeg), processed them through an AI model, and performed computer vision–based compositing/alignment with OpenCV. ",
+        "Developed secure backend endpoints and wrote unit/integration tests using ASP.NET and MongoDB.",
+        "Implemented SSO and role-based authentication for user management.",
+        "Integrated Apple Pay and Stripe gateways for secure payment processing.",
+        "Developed middleware to track and analyze usage of endpoints in the system."
       ],
     },
     {
@@ -50,9 +84,10 @@ export const siteConfig = {
       title: "Software Developer",
       dateRange: "Jan 2022 - Aug 2022",
       bullets: [
-        "Integrated backend services into a major bank's internal systems, enabling real-time data analytics.",
-        "Implemented CI/CD pipeline to reduce deployment time by 50%.",
-        "Delivered new endpoints with unit tests to ensure 99%+ reliability",
+        "Developed and tested backend endpoints with Spring Boot and PostgreSQL, achieving 99%+ reliability through comprehensive unit and integration testing.",
+        "Implemented SSO and role-based authentication, enhancing security and fine-grained access control.",
+        "Integrated backend systems with a major bank to enable secure, real-time data exchange.",
+        "Streamlined deployments by containerizing applications with Docker and automating delivery via CI/CD pipelines."
       ],
     },
   ],
@@ -65,6 +100,7 @@ export const siteConfig = {
         "Graduated Cum Laude with 2.0 GPA",
         "Thesis: Multimodal Human-AI Interaction Evaluation Framework",
         "Interdisciplinary Project: Chinese Cultural Heritage VR Game",
+        "Focus Areas: Computer Vision, High-Performance Computing, Machine Learning and Analytics."
       ],
     },
     {
@@ -72,6 +108,7 @@ export const siteConfig = {
       degree: "Bachelor of Science in Computer Engineering",
       dateRange: "2022",
       achievements: [
+        "Thesis: Live Migration using Docker Containers"
       ],
     },
   ],
